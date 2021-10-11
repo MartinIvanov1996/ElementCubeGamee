34bb30b38098c93406c79cc5165d27dde69b03f8
Martin <martinivanov1996@gmail.com>
2021-10-11 15:04:41 +0300

    Remove Unnecessary Code
    old bullet prefabs were not used so I have removed the from the code

--- Assets/Scripts/PlayerScripts/Shoot.cs
+++ Assets/Scripts/PlayerScripts/Shoot.cs
@@ -4,17 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// Does not shoot earth at the moment
+
 public class Shoot : MonoBehaviour
 {
     [SerializeField] Transform firePoint;
     [SerializeField] Transform airPoint;
     [SerializeField] Transform waterPoint;
     [SerializeField] Transform earthPoint;
-
-    [SerializeField] GameObject fireBullet;
-    [SerializeField] GameObject airBullet;
-    [SerializeField] GameObject waterBullet;
-    [SerializeField] GameObject earthBullet;
 
     [SerializeField] Button fireButton;
     [SerializeField] Button airButton;
@@ -38,7 +35,6 @@ public class Shoot : MonoBehaviour
         fireButton.onClick.AddListener(AttackFire);
         airButton.onClick.AddListener(AttackWind);
         waterButton.onClick.AddListener(AttackWater);
-        earthButton.onClick.AddListener(ShootEarth);
     }
 
     private void Update()
@@ -48,26 +44,6 @@ public class Shoot : MonoBehaviour
             fireAttackTemp.transform.position = firePoint.position;
             fireAttackTemp.transform.rotation = firePoint.rotation;
         }
-    }
-
-    private void ShootFire()
-    {
-        Instantiate(fireBullet, firePoint.position, firePoint.rotation);
-    }
-
-    private void ShootAir()
-    {
-        Instantiate(airBullet, airPoint.position, airPoint.rotation);
-    }
-
-    private void ShootWater()
-    {
-        Instantiate(waterBullet, waterPoint.position, waterPoint.rotation);
-    }
-
-    private void ShootEarth()
-    {
-        Instantiate(earthBullet, earthPoint.position, earthPoint.rotation);
     }
 
     //io marto, here are the AttackElement methods I've created. 
