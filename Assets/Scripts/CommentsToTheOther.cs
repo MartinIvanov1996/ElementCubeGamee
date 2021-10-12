690fe57e4ee12eca4219fd3c8d160278fc5b850f
Martin <martinivanov1996@gmail.com>
2021-10-12 15:30:29 +0300

    added comments script for messages

---
+++ Assets/Scripts/CommentsToTheOther.cs
@@ -0,0 +1,13 @@
+/*
+ Yo Naka tova tuka e kolkoto da si ostavqme suobshteniq, mahni emptyto deto sum go polzval da ti pokaja tozi script :D
+ 
+ Tornanodoto ima problem s collisiona koito opravih obache gornata mu chast v momenta ne bie damage. Prichinata da go napravq taka e
+ che ako zavurtish vuzduha da strelq nalqvo gornata chast na colidera vliza v poda i obekta se unishtojava a nali iskame ako zemqta 
+ dokosva element toi da izchezva. Ako opravish tornadoto da ne se vurti kogato se strelq nalqvo moje da se ostavi colider na celiq 
+ gameobject taka che tochno da si udrq kogato dokosva enemy
+ 
+ 
+ 
+ 
+ 
+ */
